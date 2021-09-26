86656ada6c5904cee1fa3fa45c9ba5f66e34448d
Miguel_Angel <mintic2021@gmail.com>
2021-09-26 15:02:53 -0500

    Creacion paquete api , clase ClientesAPI

---
+++ TiendaE-comerce/src/main/java/com/mintic/TiendaEcomerce/api/ClientesAPI.java
@@ -0,0 +1,51 @@
+package com.mintic.TiendaEcomerce.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mintic.TiendaEcomerce.dao.ClientesDao;
+import com.mintic.TiendaEcomerce.model.Clientes;
+
+
+
+
+@RestController
+@RequestMapping("clientes")
+public class ClientesAPI
+{
+	@Autowired
+	private ClientesDao clientesDao;
+	@PostMapping("/guardar")
+	public void guardar(@RequestBody Clientes clientes)
+	{
+		clientesDao.save(clientes);
+	}
+	
+	@GetMapping("/listar")
+	public List <Clientes> listar()
+	{
+		return clientesDao.findAll();
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminar(@PathVariable("id")Integer id)
+	{
+		clientesDao.deleteById(id);
+	}
+	
+	@PutMapping("/actualizar")
+	public void actualizar(@RequestBody Clientes clientes)
+	
+	{
+		clientesDao.save(clientes);
+	}
+}
