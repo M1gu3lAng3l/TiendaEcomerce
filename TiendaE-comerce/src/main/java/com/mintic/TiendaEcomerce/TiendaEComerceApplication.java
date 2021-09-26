4deba0f73ba63d2d3bf396a6f15900143e0c7fce
Miguel_Angel <mintic2021@gmail.com>
2021-09-26 14:00:11 -0500

    Configuracion Inicial Proyecto SpringBoot ciclo 3 Mintic

---
+++ TiendaE-comerce/src/main/java/com/mintic/TiendaEcomerce/TiendaEComerceApplication.java
@@ -0,0 +1,13 @@
+package com.mintic.TiendaEcomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEComerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEComerceApplication.class, args);
+	}
+
+}
---
+++ TiendaE-comerce/src/test/java/com/mintic/TiendaEcomerce/TiendaEComerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.TiendaEcomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEComerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
